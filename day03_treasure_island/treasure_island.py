56f1e8b1a7da27f85e1ea127fb64664eaee356ef
Justin Zimmerman <zimmerj271@pm.me>
2022-05-30 19:57:39 -0600

    Finished project for day 3

---
+++ day03_treasure_island/treasure_island.py
@@ -0,0 +1,48 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+print("Welcome to Treasure Island.  Your mission is to find the treasure.")
+choice = input("Left or right? ").lower()
+if choice == 'l' or choice == 'left':
+    choice = input("Swim or wait? ").lower()
+    if choice == 'w' or choice == 'wait':
+        print("There are three different colored doors.  One red, one blue and one yellow.")
+        choice = input("Which door? ").lower()
+        print(f"you chose {choice}")
+        if choice == 'r' or choice == 'red':
+            print("Burned by fire.  Game Over.")
+        elif choice == 'b' or choice == 'blue':
+            print("Eaten by beasts.  Game Over.")
+        elif choice == 'y' or choice == 'yellow':
+            print("You Win!")
+        else:
+            print("Game Over.")
+    else:
+        print("Attacked by a trout.  Game Over.")
+else:
+    print("Fall into a hole.  Game Over.")
