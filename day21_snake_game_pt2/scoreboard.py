5d4b081144b56c764b60dcc112d2fa58cfff495f
Justin Zimmerman <jzimmerman@visitpay.com>
2022-07-24 10:02:12 -0600

    Completed day 21

---
+++ day21_snake_game_pt2/scoreboard.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+POSITION = (0, 270)
+ALIGNMENT = "center"
+FONT = ('Arial', 16, 'normal')
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(POSITION)
+        self.write_score()
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.write_score()
+
+    def write_score(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
