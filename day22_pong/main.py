4d40b5f241feb10f2b4278a1027eb41a83d03bf9
Justin Zimmerman <jzimmerman@visitpay.com>
2022-07-25 07:54:53 -0600

    Finished day22

---
+++ day22_pong/main.py
@@ -0,0 +1,46 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("PONG!!!")
+screen.tracer(0)
+
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(right_paddle.up, "Up")
+screen.onkey(right_paddle.down, "Down")
+screen.onkey(left_paddle.up, "w")
+screen.onkey(left_paddle.down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # detect collision with right paddle
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # detect if paddle misses
+    if ball.distance(right_paddle) > 50 and ball.xcor() > 320:
+        scoreboard.left_point()
+        ball.reset_position()
+    if ball.distance(left_paddle) > 50 and ball.xcor() < -320:
+        scoreboard.right_point()
+        ball.reset_position()
+
+screen.exitonclick()
