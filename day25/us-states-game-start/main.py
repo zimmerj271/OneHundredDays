dd98710c659987777f8f6f08335ef7086268b319
Justin Zimmerman <jzimmerman@visitpay.com>
2022-07-27 11:04:47 -0600

    Updated US States Game

--- day25/us-states-game-start/main.py
+++ day25/us-states-game-start/main.py
@@ -1,52 +1,32 @@
 import turtle
-import pandas as pd
 from map_labeler import MapLabeler
 from score_tracker import ScoreTracker
-
-# # function to get turtle coordinate on mouse click
-# def get_mouse_click_coordinate(x, y):
-#     print(x, y)
-#
-# turtle.onscreenclick(get_mouse_click_coordinate)
-# turtle.mainloop()
-
-# def get_coordinate(df, state):
-#     guess_df = df[df['state'] == state]
-#     if len(guess_df) > 0:
-#         x = guess_df.iloc[0]['x']
-#         y = guess_df.iloc[0]['y']
-#         coordinate = (x, y)
-#         return coordinate
-#     else:
-#         return None
-
 
 screen = turtle.Screen()
 screen.title("United States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)      # Add turtle shape from image
 turtle.shape(image)         # Create new turtle object with image as shape
-states_df = pd.read_csv('50_states.csv') # dataframe with a list of states and turtle coordinates
 tracker = ScoreTracker()
 
 game_is_on = True
 while game_is_on:
     state_guess = screen.textinput(title=f"{tracker.state_count}/{tracker.total_states} Correct States",
-                                   prompt="Do you know another state?").title()
+                                   prompt="Do you know another state?")
 
-    coordinate = tracker.get_coordinate(state_guess)  # check if state guess returns a coordinate
-    if coordinate is not None:
-        labeler = MapLabeler()
-        labeler.add_state(state_guess, coordinate)
-        tracker.update_score(state_guess)
-
-    # check if all 50 states have been guessed
-    if tracker.state_count == tracker.total_states:
-        labeler = MapLabeler()
-        labeler.game_over()
+    if state_guess == "Exit" or state_guess == "exit" or state_guess is None:
         game_is_on = False
+        tracker.unnamed_states()
+    else:
+        state_guess = state_guess.title()
+        coordinate = tracker.get_coordinate(state_guess)  # check if state guess returns a coordinate
+        if coordinate is not None:
+            labeler = MapLabeler()
+            labeler.add_state(state_guess, coordinate)
+            tracker.update_score(state_guess)
 
-
-# print(states_df.head())
-
-# screen.exitonclick()
+        # check if all 50 states have been guessed
+        if tracker.state_count == tracker.total_states:
+            labeler = MapLabeler()
+            labeler.game_over()
+            game_is_on = False
--- day25/us-states-game-start/score_tracker.py
+++ day25/us-states-game-start/score_tracker.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 
-
 class ScoreTracker:
     def __init__(self):
         self.correct_states = []
         self.state_count = 0
         self.states_df = pd.read_csv('50_states.csv')  # dataframe with a list of states and turtle coordinates
+        self.all_states = self.states_df.state.to_list()
         self.total_states = len(self.states_df)
 
     def update_score(self, state):
@@ -15,10 +15,22 @@ class ScoreTracker:
 
     def get_coordinate(self, state):
         guess_df = self.states_df[self.states_df['state'] == state]
-        if len(guess_df) > 0:
+        if state in self.all_states:
             x = guess_df.iloc[0]['x']
             y = guess_df.iloc[0]['y']
             coordinate = (x, y)
             return coordinate
         else:
             return None
+
+    def unnamed_states(self):
+        unnamed_list = list(set(self.all_states) - set(self.correct_states))
+        unnamed_df = pd.DataFrame(unnamed_list, columns=["state"])
+        unnamed_df.to_csv("unnamed_states.csv", header=True, index=False)
+
+# # function to get turtle coordinate on mouse click
+# def get_mouse_click_coordinate(x, y):
+#     print(x, y)
+#
+# turtle.onscreenclick(get_mouse_click_coordinate)
+# turtle.mainloop()
